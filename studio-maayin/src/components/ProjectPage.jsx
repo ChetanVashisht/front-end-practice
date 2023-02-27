75489e408d0d04e8944eba755e399cced9c66e82
Chetan Vashisht <chetanvashisht@Chetans-Mac-mini.local>
2023-02-27 17:49:47 +0530

    v1 of the product page is ready, waiting on the girls

--- studio-maayin/src/components/ProjectPage.jsx
+++ studio-maayin/src/components/ProjectPage.jsx
@@ -5,17 +5,28 @@ import projects from '../assets/project-data'
 export default function ProjectPage() {
     const { id } = useParams()
     const project = projects.find(project => project.id == id)
-    const renderImages = (image, i) => (<img src={image.img} key={i} />)
+    const getClasses = (i) => {
+        switch (i % 10) {
+            case 0: return "wide"
+            case 3: return "tall"
+            case 8: return "wide"
+            case 9: return "tall"
+            default: return ""
+        }
+    }
+    const renderImages = (image, i) => (<img className={`img ${getClasses(i)}`} src={image.img} key={i} />)
 
     return (
         <section>
-            <div>
-                <h3> {project.type} </h3>
-                <h1> {project.name} </h1>
-                <h3> {project.date} </h3>
-                <p> {project.description} </p>
+            <div className='project-description'>
+                <div className='project-d-left'>
+                    <span> {project.type} </span>
+                    <h2> {project.name} </h2>
+                    <span> {project.date} </span>
+                </div>
+                <p className='project-d-right'> {project.description} </p>
             </div>
-            <section>{project.images.map(renderImages)}</section>
+            <section className='image-grid'>{project.images.map(renderImages)}</section>
         </section>
     )
 }
