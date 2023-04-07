644d588e0e7f5dda6263d3d611e66b309b658ef2
ChetanVashisht <chetanvashisht1994@gmail.com>
2023-04-07 21:30:41 +0530

    Fixed the about-me widget appearing on the about-me page

--- 18-simple-life/src/components/SideBar.jsx
+++ 18-simple-life/src/components/SideBar.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { RecentPostsWidget } from './RecentPosts'
 import { AboutMeWidget } from './AboutMe'
+import { Route } from 'react-router-dom'
+import { Routes } from 'react-router-dom'
 
 export default function SideBar() {
     return (
         <div className=''>
-            <AboutMeWidget />
+            <Routes>
+                <Route path="" element={<AboutMeWidget />} />
+                <Route path="about-me" element={<></>} />
+                <Route path="recent-posts" element={<AboutMeWidget />} />
+            </Routes>
             <RecentPostsWidget />
         </div>
     )
