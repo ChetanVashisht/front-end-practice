4a4d827089e635427ce4076330c4bb6983609b20
ChetanVashisht <chetanvashisht1994@gmail.com>
2023-05-22 00:00:36 +0530

    Fixed carousel and responsiveness
    Tasks remaining:
    1. Compress images and index properly
    2. Get the proper details for the first page from Divya/Mahanya
    3. Add navigation on the project page
    4. Test the same on the live site and check the routing

--- studio-maayin/src/components/ProjectsCarousel.jsx
+++ studio-maayin/src/components/ProjectsCarousel.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
-import { useState } from 'react'
 import projects from '../assets/project-data'
-import arrow from '../assets/arrow.svg'
 import Project from './Project'
 import { Link } from "react-router-dom"
 
 export default function ProjectsCarousel() {
-    const renderImages = project => (<Project project={project} key={project.id} className="slider" />)
+    const renderImages = project => (<Project project={project} key={project.id} className="slide" />)
 
     return (
         <section className='section-flex'>
             <Link to="/projects"><h2>Projects</h2></Link>
-            <div className='carousel'>
-                <div className='carousel-roll'>
+            <div className='carousel-window'>
+                <div className='carousel-container'>
                     {projects.slice(0, 8).map(renderImages)}
                 </div>
             </div>
