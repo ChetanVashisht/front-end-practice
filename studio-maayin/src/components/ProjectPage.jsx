4a6f54a72f2edcffe09212fda1993d3f21331e09
ChetanVashisht <chetanvashisht1994@gmail.com>
2023-06-29 18:33:31 +0530

    Improved image grid

--- studio-maayin/src/components/ProjectPage.jsx
+++ studio-maayin/src/components/ProjectPage.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useParams } from 'react-router-dom'
 import projects from '../assets/project-data'
 
 export default function ProjectPage() {
     const { id } = useParams()
     const project = projects.find(project => project.id == id)
+    const refs = project.images.map(_ => useRef(null))
     const getClasses = (i) => {
         switch (i % 10) {
             case 0: return "wide"
@@ -14,7 +15,19 @@ export default function ProjectPage() {
             default: return ""
         }
     }
-    const renderImages = (image, i) => (<img className={`img ${getClasses(i)}`} src={image.img} key={i} />)
+    const setClassName = (image, i) => (event) => {
+        const ref = refs[i];
+        const {naturalHeight: height, naturalWidth: width} = ref.current;
+        const determineClass = (height, width) => {
+            switch(true) {
+                case ((height - width) > 200): {return 'tall'}
+                case ((width - height) > 200): {return 'wide'}
+                default: {return 'square'}
+            }
+        }
+        ref.current.className = `img ${determineClass(height, width)}`;
+    }
+    const renderImages = (image, i) => (<img ref={refs[i]} className={`img`} src={image.img} key={i} onLoad={setClassName(image, i)} />)
 
     return (
         <main>
