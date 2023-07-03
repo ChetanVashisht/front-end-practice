18e0732639568f45bcfa4aa87d06ee56aeb56409
ChetanVashisht <chetanvashisht1994@gmail.com>
2023-07-03 22:52:10 +0530

    Basic Lightbox da

--- studio-maayin/src/components/ProjectPage.jsx
+++ studio-maayin/src/components/ProjectPage.jsx
@@ -1,9 +1,12 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import projects from '../assets/project-data'
+import * as basicLightbox from 'basiclightbox'
+import 'basicLightbox/dist/basicLightbox.min.css';
 
 export default function ProjectPage() {
     const { id } = useParams()
+    /* const [imageIndex, setImageIndex] = useState(null) */
     const project = projects.find(project => project.id == id)
     const refs = project.images.map(_ => useRef(null))
     const getClasses = (i) => {
@@ -15,10 +18,15 @@ export default function ProjectPage() {
             default: return ""
         }
     }
+    const displayImage = (image, i) => () => {
+        const ref = refs[i]
+        console.log(ref.current)
+        const img = `<img src=${ref.current.src} width="2000" height="2000"/>`
+        basicLightbox.create(img).show()
+    }
     const setClassName = (image, i) => (event) => {
         const ref = refs[i]
         const {naturalHeight: height, naturalWidth: width} = ref.current
-        console.log(height, width)
         const determineClass = (height, width) => {
             switch(true) {
                 case ((height - width) > 200): {return 'tall'}
@@ -28,8 +36,15 @@ export default function ProjectPage() {
         }
         ref.current.className = `img ${determineClass(height, width)}`;
     }
-    const renderImages = (image, i) => (<img ref={refs[i]} className={`img`} src={image.img} key={i} onLoad={setClassName(image, i)} />)
-
+    const renderImages = (image, i) => (<img ref={refs[i]} className={`img`} src={image.img} key={i} onLoad={setClassName(image, i)} onClick={displayImage(image, i)} />)
+    /* const addImageSwiper = () => {
+     *     const sides = (event) => {
+     *         displayImage()
+     *     }
+     *     window.addEventListener('keypress', sides)
+     * }
+     * useEffect(addImageSwiper, [imageIndex])
+     */
     return (
         <main>
             <section className='project-description'>
